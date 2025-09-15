3dbee2a16b611977ad444589037872ca9de29b47
Gajjar-Mohit <gajjarmohit502@gmail.com>
2025-09-15 09:56:36 +0530

    Feat: imps router

---
+++ Banks/paisa-vasool-bank/src/services/transaction.service.ts
@@ -0,0 +1,36 @@
+import prisma from "../db";
+
+export const storeTransaction = async (
+  transactionId: string,
+  amount: number,
+  type: string,
+  accountNo: string,
+  description: string
+) => {
+  console.log(`Storing transaction: ${transactionId}`);
+  try {
+    const account = await prisma.bankAccount.findFirst({
+      where: {
+        accountNo: accountNo,
+      },
+    });
+    if (!account) {
+      console.log("Account not found");
+      return;
+    }
+
+    const transaction = await prisma.transaction.create({
+      data: {
+        amount: amount,
+        account: { connect: { id: account.id } },
+        transactionId: transactionId,
+        mode: type,
+        description: description,
+      },
+    });
+    return transaction;
+  } catch (error) {
+    console.error("Error storing transaction:", error);
+    throw error;
+  }
+};
