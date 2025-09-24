d40d2d8fab2bdf9aa97826977faea1cfe7359f44
Gajjar-Mohit <gajjarmohit502@gmail.com>
2025-09-24 12:47:49 +0530

    nth go init

---
+++ nth-go/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
+}
+
+func main() {
+	message := Hello("Gladys")
+	fmt.Println(message)
+}
